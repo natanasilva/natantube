87cab024452a4c49a04df153d2a3103ddcea7ce5
Nataniel de Aguiar da Silva <nataniel.natanaguiar@gmail.com>
2021-06-05 11:18:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
